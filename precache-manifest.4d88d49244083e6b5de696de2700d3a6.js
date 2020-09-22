568161a3b7c92a4b4f938195ef7aba66e171ff41
Faiz Ahmad <55245031+faiz11amd@users.noreply.github.com>
2020-09-22 22:57:02 +0530

    Updates

---
+++ precache-manifest.4d88d49244083e6b5de696de2700d3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c1fb0b243e5255b54157d16d159610",
+    "url": "/React-Task/index.html"
+  },
+  {
+    "revision": "89644222c525ea7ee055",
+    "url": "/React-Task/static/css/main.09cf0cdc.chunk.css"
+  },
+  {
+    "revision": "c9db810bdf5721c5688c",
+    "url": "/React-Task/static/js/2.a0ba4393.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Task/static/js/2.a0ba4393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89644222c525ea7ee055",
+    "url": "/React-Task/static/js/main.2501f13d.chunk.js"
+  },
+  {
+    "revision": "93986113dd267b2f3b61",
+    "url": "/React-Task/static/js/runtime-main.bbf798e5.js"
+  }
+]);
